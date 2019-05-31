431e64ad1f2174467927766040613d5bed4fdc68
Katja Berčič <katja.bercic@gmail.com>
2019-05-31 13:42:21 +0200

    Add fontawesome to frontend, icons for dataset/system tags

--- frontend/src/Tags.js
+++ frontend/src/Tags.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Badge } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDatabase, faTable } from '@fortawesome/free-solid-svg-icons'
 import { compareObjects } from './util.js';
+
+const typeOrder = (cat) => {
+    if (cat === "category") return 1;
+    if (cat === "MO") return 2;
+    else return 3;
+}
+
+function Category(props) {
+    var icon = faTable;
+    if (props.label === "system") icon = faDatabase;
+    return <Badge color="primary" pill><FontAwesomeIcon icon={icon} /></Badge>
+}
 
 function Tag(props) {
     var style = "light";
     if (props.type === "MO") style = "info";
+    if (props.type === "category") return <Category label={props.label} />
     return <Badge color={style} pill>{props.label}</Badge>
 }
 
 export default function Tags(props) {
     const tagArray = props.value;
-    tagArray.sort((a, b) => compareObjects(a, b, (o) => o.name));
+    tagArray.sort((a, b) => compareObjects(a, b, (o) => [typeOrder(o.type), o.name]));
     const tags = tagArray.map((t, i) => <Tag key={i} label={t.name} type={t.type} />)
     return <React.Fragment>{tags}</React.Fragment>
 }
--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,6 +1,12 @@
 export const compareObjects = (a, b, compareBy) => {
     var left = compareBy(a);
     var right = compareBy(b);
+    if (!!left && left.constructor === Array) { // is array
+        var i = 0;
+        while (i < left.length && left[i] === right[i]) { i++; }
+        left = left[i];
+        right = right[i];
+    }
     if (left === right) return 0;
     else return (left < right ? -1 : 1);
 }
