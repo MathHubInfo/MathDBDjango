ce7e6dd5a56a27291b9407c2bddc5977ff519bac
Katja Berčič <katja.bercic@gmail.com>
2023-12-13 18:53:57 +0100

    Update App.js with link to new website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,6 +37,8 @@ class App extends Component {
                 <Row>
                     <Col>
                         <h1>Catalogue of Mathematical Datasets</h1>
+                        <p>I'm happy to report that the catalogue has found new friends and a new home at <a href="https://mathbases.org">MathBases.org</a>. 
+                        This page will remain here as a historical record.</p>
                         <p>See the <a href="https://docs.mathhub.info/data/Math-Databases">MathHub Documentation</a> for the non-tabulated contents as well as for more information.</p>
                         <p>The information in this catalogue is incomplete, please <a href="https://www.fmf.uni-lj.si/en/directory/742/bercic-katja/">help me fill it in</a>.</p>
 
